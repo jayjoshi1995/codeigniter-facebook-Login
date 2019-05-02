822a976914c9e43030a7aeae8eb025ea9a3675ba
jayjoshi1995 <jay.joshi789@gmail.com>
2019-05-02 11:35:19 +0530

    facebook login plugin

---
+++ application/controllers/Login.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Login extends CI_Controller {
+	
+	function __construct() {
+		parent::__construct();
+		$this->load->library('facebook');
+    }
+	
+	public function index(){
+		$userData = array();
+		if($this->facebook->is_authenticated()){
+			$userProfile = $this->facebook->request('get', '/me?fields=id,first_name,last_name,email,gender,locale,picture');
+			echo "<pre>";
+			print_r($userProfile);
+			die;
+		}
+		else
+		{
+            $data['authUrl'] =  $this->facebook->login_url();
+        }
+	    $this->load->view('login',$data);
+    }
+
+	public function logout() {
+		$this->facebook->destroy_session();
+		redirect('/login');
+    }
+}
